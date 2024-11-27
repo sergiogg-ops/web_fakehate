0c9d0ba1de03a92fc5b7106e26ce8a6d6892a6ee
sergiogg-ops <sergiogg1yo@gmail.com>
2024-11-27 14:13:12 +0100

    creacion del socket

--- restart.sh
+++ restart.sh
@@ -9,6 +9,8 @@ sleep 1
 
 # Check if socket file exists, then change permissions
 if [ -e flaskapp.sock ]; then
+    sudo chown sergio:www-data flaskapp.sock
+    chmod 755 .
     chmod 777 flaskapp.sock
 else
     echo "Socket file flaskapp.sock not found."
